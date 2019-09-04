8f0e19a8e1ac8497aba27bbaebe643aeefb3b94e
Amruth Nare <amruthnare.1@gmail.com>
2019-09-04 17:53:23 -0400

    setup mongo atlas

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+dotenv.config()
+
+const app = express();
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+
+
+const ur = process.env.URI 
+mongoose.connect(uri, {useNEwUrlParser: true, useCreateIndex: true})
+const connection = mongoose.connection
+connection.once('open', () => {
+    console.log("Connected to Mongo")
+})
+
+
+
+
+
+
+
+
+app.listen(port, () =>{
+    console.log("Server running at: http://localhost:" + port)
+})
